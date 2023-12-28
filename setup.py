2e2b12ca2675e4c84ac84fe1337a659254f568fb
maercaestro <maercaestro@gmail.com>
2023-12-28 12:08:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,39 @@
-from setuptools import find_packages, setup
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
 
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
 setup(
-    name = 'refutaslib',
-    packages = find_packages(),
-    version='0.1.0',
-    description='Refutas Python Library for viscosity of mixture',
-    author='Abu Huzaifah Bidin',
+    name="RefutasLibrary",
+    version="0.1.0",
+    description="Python Package Library for Viscosity of a mixture ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Abu Huzaifah Bidin",
+    author_email="maercaestro@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=["RefutasLibrary"],
+    include_package_data=True,
+    install_requires=["numpy"]
 )
